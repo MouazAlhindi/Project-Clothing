536a29d6c2d7f4fc9dbef04fe104d4d9f8f77a6b
Mouaz Alhindi <mozalhindi@gmail.com>
2019-02-05 20:34:03 -0500

    Initalized Project infrastructure

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Library Import
+const express = require('express');
+const mongo = require('mongodb')
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+// Mongo DB test
+var MongoClient = mongo.MongoClient;
+const url = "mongo://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
